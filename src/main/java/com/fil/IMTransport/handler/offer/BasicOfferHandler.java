a3b39d5106f6be2d3d82e3e58a374fc95248248f
elkhantachei <ismail.elkhantache@amplexor.com>
2019-03-24 21:17:19 +0100

    Check if the line is inaccessible or not

--- src/main/java/com/fil/IMTransport/handler/offer/BasicOfferHandler.java
+++ src/main/java/com/fil/IMTransport/handler/offer/BasicOfferHandler.java
@@ -1,9 +1,10 @@
 package com.fil.IMTransport.handler.offer;
 
-import com.fil.IMTransport.object.Line;
-import com.fil.IMTransport.object.Offer;
-import com.fil.IMTransport.object.Station;
-import com.fil.IMTransport.object.Trip;
+import com.fil.IMTransport.Services.BookingService;
+import com.fil.IMTransport.Services.LineInnaccessibilityService;
+import com.fil.IMTransport.Services.LineService;
+import com.fil.IMTransport.object.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +13,25 @@ import static com.fil.IMTransport.object.Global.NB_PASSENGERS_BY_TRAIN;
 
 public class BasicOfferHandler implements OfferHandler {
 
-    public List<Line> allLines = new ArrayList<>();
+    @Autowired
+    private BookingService bookingService;
+    @Autowired
+    private LineService lineService;
+    @Autowired
+    private LineInnaccessibilityService lineInnaccessibilityService;
 
     @Override
     public List<Trip> handle(List<Offer> offers) {
-        allLines.add(new Line(new Station("A"), new Station("B"), 50));
-        allLines.add(new Line(new Station("B"), new Station("C"), 50));
 
         ArrayList<Trip> trips = new ArrayList<>();
         for (Offer o : offers) {
             Trip t = new Trip();
             ArrayList<Line> lines = new ArrayList<>();
             t.setLines(this.getLines(o.getStartStation(), o.getEndStation()));
-            t.setNbTrain((int)this.getNbTrainFromPassenger(o.getNbPassengers()));
+            t.setNbTrain((int) this.getNbTrainFromPassenger(o.getNbPassengers()));
             String affichageChemin = "";
-            for (Line l :  t.getLines()){
-                affichageChemin += l.getStart().getName() + " => " + l.getEnd().getName()+" , ";
+            for (Line l : t.getLines()) {
+                affichageChemin += l.getStart().getName() + " => " + l.getEnd().getName() + " , ";
             }
             System.out.println("PATH : " + affichageChemin);
 
@@ -38,21 +42,21 @@ public class BasicOfferHandler implements OfferHandler {
         return trips;
     }
 
-    private double getNbTrainFromPassenger(int nbPassenger){
+    private double getNbTrainFromPassenger(int nbPassenger) {
         double nbPbyT = NB_PASSENGERS_BY_TRAIN;
-        return Math.ceil(nbPassenger/nbPbyT);
+        return Math.ceil(nbPassenger / nbPbyT);
     }
 
 
-    private List<Line> getLines(Station start, Station end){
+    private List<Line> getLines(Station start, Station end) {
         boolean found = false;
         List<Line> lines = new ArrayList<>();
         Station lookingFor = start;
-        while(!found){
+        while (!found) {
             Line l = this.getLineFromStartStation(lookingFor);
             lines.add(l);
             System.out.println(l.getStart().getName() + " => " + l.getEnd().getName());
-            if (l.getEnd().getName().equals(end.getName())){
+            if (l.getEnd().getName().equals(end.getName()) && l.getInaccessibilities().size() == 0) {
                 System.out.println("FOUND !");
                 found = true;
             }
@@ -61,9 +65,9 @@ public class BasicOfferHandler implements OfferHandler {
         return lines;
     }
 
-    private Line getLineFromStartStation(Station start){
-        for (Line l : allLines){
-            if (l.getStart().getName().equals(start.getName())){
+    private Line getLineFromStartStation(Station start) {
+        for (Line l : lineService.getLines()) {
+            if (l.getStart().getName().equals(start.getName())) {
                 return l;
             }
         }
