a78b6cfba8e0bd1d7b65af9119247ca425e92c72
OcéaneB <oce88b@gmail.com>
2019-03-24 15:21:57 +0100

    Modif algos kafka

--- src/main/java/com/fil/IMTransport/kafka/KafkaService.java
+++ src/main/java/com/fil/IMTransport/kafka/KafkaService.java
@@ -10,14 +10,16 @@ import org.springframework.messaging.SubscribableChannel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fil.IMTransport.ImTransportApplication;
+import com.fil.IMTransport.object.Offer;
 
 public class KafkaService {
 
+	private static ObjectMapper mapper = new ObjectMapper();
+	
 	private KafkaService() {
 	}
 
 	public static void askForBooking(BookingRequest request) {		
-		ObjectMapper mapper = new ObjectMapper();
 		try {
 			ImTransportApplication.kafkaTemplate.send("request", mapper.writeValueAsString(request));
 			System.out.println("Booking request sent: "+mapper.writeValueAsString(request));
@@ -35,5 +37,8 @@ public class KafkaService {
 	@KafkaListener(topics = "offers", groupId = "transport")
 	public static void listenOffers(String message) throws IOException {
 		System.out.println("Received message in offer: " + message);
+		Offer offer = mapper.readValue(message, Offer.class);
+		//add To BDD
+		//appel algo pour calculer la course
 	}
 }
