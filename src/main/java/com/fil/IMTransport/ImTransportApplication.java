ad3be92e3b1e6ef09184fa414bb6e59028b44021
Lucille <lucille.gillard@macroprog.com>
2019-03-25 15:18:47 +0100

    correction bdd

--- src/main/java/com/fil/IMTransport/ImTransportApplication.java
+++ src/main/java/com/fil/IMTransport/ImTransportApplication.java
@@ -1,24 +1,10 @@
 package com.fil.IMTransport;
 
-import java.awt.List;
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fil.IMTransport.handler.offer.BasicOfferHandler;
-import com.fil.IMTransport.kafka.BookingRequest;
-import com.fil.IMTransport.kafka.KafkaService;
-import com.fil.IMTransport.object.Offer;
-import com.fil.IMTransport.object.Station;
 
 @SpringBootApplication
 public class ImTransportApplication {
@@ -29,9 +15,4 @@ public class ImTransportApplication {
 	public static void main(String[] args) {
         SpringApplication.run(ImTransportApplication.class, args);
     }
-	
-	public void run(ApplicationArguments args) throws Exception {
-
-	}
-
 }
